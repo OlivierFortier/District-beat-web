6b0201e965f322cf1942a8c0d9fafbb4a590da5a
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-16 17:05:54 -0500

    site web de base terminé

--- src/views/Apropos.vue
+++ src/views/Apropos.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="about">
-    <h1>À propos du jeu</h1>
-    <h2>Créé par : </h2>
-    <div class="lists">
-      <ul class="nes-list is-circle">
-        <li>Bernard Dwyer-Beaudet</li>
-        <li>Olivier Fortier</li>
-        <li>Alejandra Damaris Miranda Reyes</li>
-        <li>Thomas Lorenzo</li>
-      </ul>
+    <div class="nes-container is-rounded is-dark">
+      <h1>À propos du jeu</h1>
+      <h2>Créé par :</h2>
+      <div class="lists">
+        <ul class="nes-list is-circle">
+          <li>Bernard Dwyer-Beaudet</li>
+          <li>Olivier Fortier</li>
+          <li>Alejandra Damaris Miranda Reyes</li>
+          <li>Thomas Lorenzo</li>
+        </ul>
+      </div>
+      <h2>Un peu d'info...</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+        voluptate excepturi earum doloremque delectus error! Magnam placeat eius,
+        aut amet at veniam impedit mollitia libero ipsum, corrupti, reiciendis
+        laborum nesciunt.
+      </p>
     </div>
   </div>
 </template>
@@ -17,5 +26,6 @@
 .about {
   margin-top: 5%;
   color: white;
+  width: 80%;
 }
 </style>
