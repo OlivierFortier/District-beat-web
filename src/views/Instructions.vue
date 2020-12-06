54b37bc961fb56a470093ffd772985296358af99
Olivier Fortier <olivier_fortier@outlook.com>
2020-12-06 11:07:25 -0500

    ajout boutons

--- src/views/Instructions.vue
+++ src/views/Instructions.vue
@@ -2,12 +2,18 @@
   <main>
     <div class="nes-container is-rounded is-dark">
       <h1>Instructions</h1>
-      <h2>But du jeu</h2>
+      <h2>But du jeu <i class="nes-icon trophy is-large"></i></h2>
       <p>
         Vous devez survivre et vous battre à travers des hordes d'ennemis de
         quartier en quartier, afin d'arriver à la fin ou se situe le concert.
       </p>
-      <h2>Contrôles</h2>
+      <h2>Contrôles <i class="snes-logo"></i></h2>
+      <button type="button" class="nes-btn is-success">A</button>
+      <button type="button" class="nes-btn is-error">B</button>
+      <button type="button" class="nes-btn is-primary">X</button>
+      <button type="button" class="nes-btn is-warning">Y</button>
+      <button type="button" class="nes-btn">L</button>
+      <button type="button" class="nes-btn">R</button>
     </div>
   </main>
 </template>
