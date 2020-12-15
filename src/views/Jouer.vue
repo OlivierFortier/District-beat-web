dd095f182915a6e457391ba6bd4f350bcdac2751
Olivier Fortier <olivier_fortier@outlook.com>
2020-12-15 02:11:54 -0500

    ajustement site jeu

--- src/views/Jouer.vue
+++ src/views/Jouer.vue
@@ -2,7 +2,7 @@
   <main class="nes-container is-rounded is-dark">
     <iframe
       frameborder="0"
-      src="https://itch.io/embed-upload/2949239?color=333333"
+      src="https://itch.io/embed-upload/3102515?color=333333"
       allowfullscreen=""
       width="1024"
       height="788"
